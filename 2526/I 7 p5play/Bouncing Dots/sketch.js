817136eed187618e842a94521148a5fc23f029f1
Andy Lightcap <andy.lightcap@gmail.com>
2025-08-18 14:34:09 -0400

    2526 I 7 Lesson 2

--- 2526/I 7 p5play/Bouncing Dots/sketch.js
+++ 2526/I 7 p5play/Bouncing Dots/sketch.js
@@ -2,24 +2,46 @@ new Q5();
 
 new Canvas(800, 600);
 
-let dot = new Sprite();
-dot.color = 'red';
-dot.radius = 25;
-dot.x = halfWidth;
-dot.y = halfHeight;
+let colors = ['red', 'green', 'blue'];
 
-dot.direction = 180;
-dot.speed = 5;
+createDot();
+createDot();
+createDot();
 
 function update() {
 	background('skyblue');
+}
 
-	// bounce off the left side of the canvas
-	if (dot.x < 0 + dot.radius) {
-		dot.vel.x *= -1;
+function createDot() {
+	let dot = new Sprite();
+	dot.color = random(colors);
+	dot.radius = 25;
+	dot.x = halfWidth;
+	dot.y = halfHeight;
+
+	dot.direction = random(360);
+	dot.speed = random(5, 10);
+
+	dot.overlaps(allSprites);
+
+	dot.update = () => {
+		// bounce off the left side of the canvas
+		if (dot.x < 0 + dot.radius) {
+			dot.vel.x *= -1;
+		}
+
+		if (dot.x > width - dot.radius) {
+			dot.vel.x *= -1;
+		}
+
+		if (dot.y < 0 + dot.radius) {
+			dot.vel.y *= -1;
+		}
+
+		if (dot.y > height - dot.radius) {
+			dot.vel.y *= -1;
+		}
 	}
 
-	if (dot.x > width - dot.radius) {
-		dot.vel.x *= -1;
-	}
+	return dot;
 }
