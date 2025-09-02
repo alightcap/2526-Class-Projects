4ea35600a1a692f067fbe846fb4421e589a42bbe
Andy Lightcap <andy.lightcap@gmail.com>
2025-09-02 12:10:00 -0400

    2526 F 7 Maze Runner Lesson 2

--- 2526/F 7 p5play/2526 F Maze Runner/sketch.js
+++ 2526/F 7 p5play/2526 F Maze Runner/sketch.js
@@ -24,14 +24,53 @@ function createPlayer() {
 	let s = new Sprite();
 	s.addAni('idle', playerIdle);
 	s.addAni('walk', playerWalk);
-	s.scale = 2;
 
+	s.scaleValue = 2;
 	s.walkSpeed = 5;  // this is a custom property
 
+	s.scale = s.scaleValue;
+
 	s.update = () => {
-		if (kb.pressing('left')) {
-			s.vel.x = -1 * s.walkSpeed;
+		let inputVector = s.getInput();
+
+		s.visuals();
+
+		s.vel.x = inputVector.x * s.walkSpeed;
+		s.vel.y = inputVector.y * s.walkSpeed;
+	}
+
+	s.visuals = () => {
+		if (s.vel.x < 0) {
+			s.scale.x = -s.scaleValue;
 		}
+
+		if (s.vel.x > 0) {
+			s.scale.x = s.scaleValue;
+		}
+	}
+
+	s.getInput = () => {
+		let inputVector = createVector(0, 0);
+
+		if (kb.pressing('left')) { // both a and the left arrow work.
+			inputVector.x = -1;
+		}
+
+		if (kb.pressing('right')) {
+			inputVector.x = 1;
+		}
+
+		if (kb.pressing('up')) {
+			inputVector.y = -1;
+		}
+
+		if (kb.pressing('down')) {
+			inputVector.y = 1;
+		}
+
+		inputVector.normalize();
+
+		return inputVector;
 	}
 
 	return s;
