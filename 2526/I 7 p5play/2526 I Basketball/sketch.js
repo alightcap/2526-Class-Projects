6e865dd209e5e56660df363423315153e987fcb4
Andy Lightcap <andy.lightcap@gmail.com>
2025-09-29 14:32:57 -0400

    2526 7 I Basketball Lesson 3

--- 2526/I 7 p5play/2526 I Basketball/sketch.js
+++ 2526/I 7 p5play/2526 I Basketball/sketch.js
@@ -26,17 +26,22 @@ function preload() {
     basketballImg = loadImage('assets/basketball.png');
 }
 
+
 function setup() {
     player = createPlayer();  // call the function that creates the player sprite, and store the sprite here.
     ground = createGround();
     basketball = createBasketball();
+    createWalls();
 
     player.overlaps(basketball, player.getBall);
+    player.overlapped(basketball, player.canShoot);
 }
+
 
 function update() {
     background('skyblue');
 }
+
 
 function createBasketball() {
     let b = new Sprite();
@@ -45,9 +50,11 @@ function createBasketball() {
     b.radius = 5;
     b.scale = 2;
     b.bounciness = 0.5;
+    b.mass = 1;
 
     return b;
 }
+
 
 function createGround() {
     let g = new Sprite();
@@ -63,19 +70,23 @@ function createGround() {
     return g;
 }
 
+
 function createPlayer() {  // set up and return the player sprite
     let p = new Sprite();
     // p.debug = true;
     p.width = 5;
     p.height = 8;
+    p.mass = 10;
 
     p.size = 5;
     p.scale = p.size;
     p.bounciness = 0;
     p.rotationLock = true;
 
-    p.jumpForce = 30;
+    p.jumpForce = 5000;
+    p.shootForce = 500;
     p.bearing = -90;
+    p.isShooting = false;
 
     p.moveSpeed = 5;
 
@@ -96,6 +107,24 @@ function createPlayer() {  // set up and return the player sprite
                 p.applyForce(p.jumpForce);
             }
         }
+        if (kb.presses('space')) {  // factor this out into a function
+            // print(p.joints[0]['spriteB']);
+            if (p.joints[0]) {  // is there a joint here?
+                p.isShooting = true;
+                let ball = p.joints[0]['spriteB'];
+                let bearing = -60;
+                let torque = -1;
+                if (p.scale.x < 0) {
+                    bearing = -120;
+                    torque = 1;
+                }
+
+                p.joints[0].delete();
+                ball.bearing = bearing;
+                ball.applyForce(p.shootForce);
+                ball.applyTorque(torque);
+            }
+        }
 
         p.vel.x = inputX * p.moveSpeed;
 
@@ -113,11 +142,38 @@ function createPlayer() {  // set up and return the player sprite
         }
     }
 
+    p.canShoot = () => {
+        p.isShooting = false;
+    }
+
     p.getBall = (player, basketball) => {
-        basketball.x = p.x;
-        basketball.y = p.y;
-        new GlueJoint(p, basketball);
+        if (!p.isShooting) {
+            basketball.x = p.x;
+            basketball.y = p.y;
+            new GlueJoint(p, basketball);
+        }
     }
 
     return p;
+}
+
+
+function createWalls() {
+    let left = new Sprite();
+    left.x = 0;
+    left.y = halfHeight;
+    left.width = 20;
+    left.height = height;
+    left.physics = 'STATIC';
+    left.friction = 0;
+    left.visible = false;
+
+    let right = new Sprite();
+    right.x = width;
+    right.y = halfHeight;
+    right.width = 20;
+    right.height = height;
+    right.physics = 'STATIC';
+    right.friction = 0;
+    right.visible = false;
 }
