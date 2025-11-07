897d3de82dc91768478a73ed96be8fd672e2c471
Andy Lightcap <andy.lightcap@gmail.com>
2025-11-07 11:13:50 -0500

    2526 7 I Breakout added exploding bricks

--- 2526/I 7 p5play/2526 I Breakout/src/brick.js
+++ 2526/I 7 p5play/2526 I Breakout/src/brick.js
@@ -7,21 +7,43 @@ bricks.height = 20;
 bricks.tile = '-';
 bricks.pointValue = 100;
 
+let explodingBricks = new bricks.Group();  // subgroup
+explodingBricks.color = 'red';
+explodingBricks.canExplode = true;
+explodingBricks.explosionRadius = 60;
+explodingBricks.tile = 'x';
+
 let tiles = new Tiles(
     [
-        '---------------',
+        '--x---------x--',
         '-----.....-----',
         '---.........---',
         '-.............-',
         '---.........---',
         '-----.....-----',
-        '---------------',
+        '--x---------x--',
     ],
     90, 100,
     bricks.width + 4, bricks.height + 4
 );
 
-bricks.onHit = (brick) => {
+bricks.onHit = async (brick) => {
     brick.delete();
+    if (brick.canExplode) {
+        explodingBricks.onHit(brick);
+    }
 }
-// let brick = new bricks.Sprite();
+
+explodingBricks.onHit = async (brick) => {
+    let explosion = new Sprite();
+    explosion.radius = brick.explosionRadius;
+    explosion.color = 'red';
+    explosion.x = brick.x;
+    explosion.y = brick.y;
+    explosion.physics = 'NONE';
+
+    bricks.overlaps(explosion, bricks.onHit);
+
+    await delay(50);
+    explosion.delete();
+}
